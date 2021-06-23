0c07c9d5455d5de4299b8afaeb5efedc32e63fca
Diego Ventura <diegoventurasg@gmail.com>
2021-06-23 13:18:07 -0300

    Add routes

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/modules/login/login_page.dart';
 import 'package:payflow/modules/splash/splash_page.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
+
+import 'modules/home/home_page.dart';
 
 class AppWidget extends StatelessWidget {
   @override
@@ -9,7 +12,12 @@ class AppWidget extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Pay Flow',
       theme: ThemeData(primaryColor: AppColors.primary),
-      home: SplashPage(),
+      initialRoute: "/splash",
+      routes: {
+        "/splash": (context) => SplashPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
