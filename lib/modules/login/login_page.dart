532fd54d733ab4716888fcca54388856fcba6894
Diego Ventura <diegoventurasg@gmail.com>
2021-06-23 13:24:53 -0300

    Create LoginController

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:payflow/modules/login/login_controller.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
@@ -12,6 +14,7 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final controller = LoginController();
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -61,11 +64,9 @@ class _LoginPageState extends State<LoginPage> {
                       right: 40,
                       top: 40,
                     ),
-                    child: SocialLoginButton(
-                      onTap: () {
-                        print('clicou');
-                      },
-                    ),
+                    child: SocialLoginButton(onTap: () {
+                      controller.googleSignIn(context);
+                    }),
                   ),
                 ],
               ),
